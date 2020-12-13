0f858c5bf9cb824083822bff715449b9f78378c1
Connor Barrett <connor.steven.barrett@gmail.com>
2020-12-13 13:53:51 -0600

    building contact page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
 //   plugins: ["gatsby-plugin-postcss", "gatsby-plugin-styled-components"],
+   pathPrefix: "/solistic-healing-website",
    plugins: ["gatsby-plugin-styled-components"]
 };
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import Logo from '../components/Navbar/Logo'
 import Navbar from "../components/Navbar/Navbar"
+import '../styles/contact.css'
 
 // styles
 const pageStyles = {
@@ -12,6 +13,13 @@ const pageStyles = {
 const Contact = () => (
   <div style={pageStyles}>
    <Navbar/>
+   <div class="menu">hello this is a menu</div>
+   <form>
+		<input name="name" placeholder="What is your name?" class="name" required />
+		<input name="emailaddress" placeholder="What is your email?" class="email" type="email" required />
+    <textarea rows="4" cols="50" name="subject" placeholder="Please enter your message" class="message" required></textarea>
+    <input name="submit" class="btn" type="submit" value="Send" />
+   </form>  
   </div>
 )
 
