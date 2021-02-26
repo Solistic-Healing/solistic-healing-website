abd5b84c035da651f6465fea0cafef0f19289cc1
Connor Barrett <connor.steven.barrett@gmail.com>
2021-02-26 02:18:29 -0600

    removed opacity on form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -67,36 +67,24 @@ const Contact = () => (
       </div>
       <BottomBar />
    </div>
-
-
-   //   <div style={pageStyles}>
-   //    <div style={NavStyle}>
-   //       <Navbar/>
-
-   //    </div>
-   //    {/* TAB TITLE */}
-   //    <title>CONTACT</title>
-   //    {/* PAGE HEADING */}
-   //    <div style={TitleContentStyles}>
-   //       <h1 style={TitleHeadingStyles}>
-   //          Contact
-   //       </h1>
-   //    </div>
-   //    <div class="twoboxes">
-   //       <div class="form">
-   //          <form action="https://formspree.io/f/xpzoenvd" method="post">
-   //             <input name="name" placeholder="What is your name?" class="name" required />
-   //             <input name="emailaddress" placeholder="What is your email?" class="email" type="email" required />
-   //             <textarea rows="4" cols="50" placeholder="Please enter your message" class="message" required></textarea>
-   //             <input name="submit" style={btn} type="submit" value="Send" />
-   //          </form>
-   //       </div>
-   //       <div class="location">
-   //          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2820.862041301893!2d-93.17051068461625!3d45.00742337909819!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x52b32b96aa165c91%3A0xff67265cf803d1f8!2s1611%20County%20B%20Rd%20W%20%23214%2C%20Roseville%2C%20MN%2055113!5e0!3m2!1sen!2sus!4v1612138864194!5m2!1sen!2sus" width="600" height="400" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-   //       </div>
-   //    </div>
-   //    <BottomBar/>
-   //   </div>
 )
 
 export default Contact;
+
+const form = {
+   
+}
+
+const primaryContentStyles = {
+   margin: 'auto',
+   marginTop: '10px',
+   maginBottom: '50px',
+   width: '86%',
+   border: '1px solid gray',
+   backgroundColor: 'black',
+   opacity: '90%',
+   borderRadius: '5px',
+   padding: '10px',
+   marginBottom: '75px'
+}
+
